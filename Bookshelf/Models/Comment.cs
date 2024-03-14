2b099e4d0bf051a314a51fc1c0ea8e37ac021271
Aziz Kızgın <azizkzgn@gmail.com>
2024-03-14 23:46:52 +0300

    feat: UpdatedAt added

--- Bookshelf/Models/Comment.cs
+++ Bookshelf/Models/Comment.cs
@@ -10,6 +10,7 @@ namespace Bookshelf.Models
         public int Id { get; set; }
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; } = null;
         public int? BookId { get; set; }
         public Book? Book { get; set; }
     }
