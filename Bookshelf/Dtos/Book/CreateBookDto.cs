f16544ca87c558efc4a61e3549f28e53be0076d7
Aziz Kızgın <azizkzgn@gmail.com>
2024-03-17 01:01:34 +0300

    feat: CreateBookDto updated

--- Bookshelf/Dtos/Book/CreateBookDto.cs
+++ Bookshelf/Dtos/Book/CreateBookDto.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bookshelf.Dtos.Comment;
+using Bookshelf.Models;
 
 namespace Bookshelf.Dtos.Book
 {
     public class CreateBookDto
     {
         public string Title { get; set; } = string.Empty;
+        public string Comment { get; set; } = string.Empty;
     }
 }
