10968c4e808aee630f0365848ae488ee95b2b894
Aziz Kızgın <azizkzgn@gmail.com>
2024-03-17 01:42:21 +0300

    feat: CommentQueryObject updated

--- Bookshelf/Helpers/CommentQueryObject.cs
+++ Bookshelf/Helpers/CommentQueryObject.cs
@@ -7,7 +7,9 @@ namespace Bookshelf.Helpers
 {
     public class CommentQueryObject
     {
-        public int BookId { get; set; }
+        public int? BookId { get; set; }
+        public string? Content { get; set; }
+        public string? UserId { get; set; }
         public bool OrderByFavorite { get; set; } = false;
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 15;
