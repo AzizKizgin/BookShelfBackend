af2c831f2e6ad9513ef51ff81b4499909359106d
Aziz Kızgın <azizkzgn@gmail.com>
2024-03-17 13:37:07 +0300

    feat: migration

---
+++ Bookshelf/Migrations/20240317002510_BookComments.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bookshelf.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Books_BookId",
+                table: "Comments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2edaa3e1-da29-47cc-9050-784c0e16650b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6e3e6460-501b-4d85-877b-8ad274c21445");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "132a94a3-715d-4a48-8a6a-d2e0650fdfec", null, "Admin", "ADMIN" },
+                    { "196e7a66-a7ad-4f64-8e63-049ab89ea1cd", null, "User", "USER" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Books_BookId",
+                table: "Comments",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Books_BookId",
+                table: "Comments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "132a94a3-715d-4a48-8a6a-d2e0650fdfec");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "196e7a66-a7ad-4f64-8e63-049ab89ea1cd");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2edaa3e1-da29-47cc-9050-784c0e16650b", null, "Admin", "ADMIN" },
+                    { "6e3e6460-501b-4d85-877b-8ad274c21445", null, "User", "USER" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Books_BookId",
+                table: "Comments",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id");
+        }
+    }
+}
