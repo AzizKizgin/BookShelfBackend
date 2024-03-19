5a87ce935ccc6bbb862255fb155aa11173d3a93e
Aziz Kızgın <azizkzgn@gmail.com>
2024-03-19 21:51:38 +0300

    feat: SwaggerDoc title changed

--- Bookshelf/Program.cs
+++ Bookshelf/Program.cs
@@ -17,7 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
 {
-    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Bookshelf", Version = "v1" });
     option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
